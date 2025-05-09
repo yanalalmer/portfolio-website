0f698f52e7228bc398ab60b2ecd1d0e64f7511ba
yanalalmer <yanalalmer@yahoo.com>
2025-05-09 09:26:32 +0200

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    TITAN_EMAIL: process.env.TITAN_EMAIL,
+    TITAN_PASSWORD: process.env.TITAN_PASSWORD,
+  },
+};
 
 export default nextConfig;
